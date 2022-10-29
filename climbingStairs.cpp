bfcb57b4a35b83e86538aa415df3936941336750
Jubayer Ahmmod <jubayer15-10807@diu.edu.bd>
2022-10-29 20:58:21 +0600

    solving

--- climbingStairs.cpp
+++ climbingStairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[45];
+        int dp[47];
       dp[1]=1;
         dp[2]=2;
         for(int i=3; i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
