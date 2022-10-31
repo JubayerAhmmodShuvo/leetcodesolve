950d7c12f0276a6a39b90abaec59a32918e5fddf
Jubayer Ahmmod <jubayer15-10807@diu.edu.bd>
2022-10-31 14:26:17 +0600

    practicing

--- ValidPalindrome2.cpp
+++ ValidPalindrome2.cpp
@@ -27,3 +27,45 @@ public:
         return true;
     }
 };
+
+// Path: ValidPalindrome.cpp
+// int i=0;
+// int j=s.size()-1;
+// int cnt=0;
+// while(i<j){
+//   if(s[i]==s[j]){
+//     i++;
+//     j--;
+//   }else{
+//     cnt++;
+//     i++;
+//   }
+//   if(cnt<1){
+//     break;
+//   }
+// }
+
+// int s=0;
+// int e=s.size()-1;
+// int cnt1=0;
+// while(s<n){
+//   if(s[i]==s[j]){
+//     s++;
+//     e--;
+
+//   }else{
+//     cnt1++;
+//     e--;
+//   }
+//   if(cnt1<1){
+//     break;
+//   }
+// }
+
+// if(cnt==1 || cnt1==1)
+// return true;
+
+// if(cnt==0 || cnt == 0)
+// return true;
+
+// return false;
