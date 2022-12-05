92cce4454eaaae4dd11992596eb2a1fe2b2c17b9
Jubayer Ahmmod <jubayer15-10807@diu.edu.bd>
2022-12-05 19:02:09 +0600

    practicing algorithm l

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -22,22 +22,41 @@ public:
 /* 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-     int i=0, j=nums.size()-1, pos=-1;
-        while(i<=j){
-           int mid=i+(j-i)/2;
-            if(nums[mid]==target){
-              pos=mid;
-               break;
-               }
-            else if(nums[mid]>j){
-              j=mid-1;
-              }
-            else{
-              i=mid+1;
-              }
-        }
-        return pos;
-		
-	}
-}; */
+    int search(vector<int>& nums, int target)  {
+        int s=0;int e=nums.size()-1;int mid=s+(e-s)/2;
+
+        while(s<=e)
+        {
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                e=mid-1;
+            }
+            else
+            {
+                s=mid+1;
+            }
+            mid=s+(e-s)/2;
+        }return -1;
+    }
+};
+// int s=0, e=nums.size()-1, mid=s+(e-s)/2;
+// while(s<=e){
+//   if(nums[mid]==target){
+//     return mid;
+
+//   }
+//   else if(nums[mid]>target){
+//     emid-1;
+//   }
+//   else {
+//     s=mid+1;
+//   }
+//   mid=s+(e-s)/2;
+// }
+// return -1;
+
+ */
